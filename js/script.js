c8df54ab40dcc61084fa4dbd70cf8ebfddce2940
Jakir Hasan Mridul <jakirhasan.mridul@gmail.com>
2023-09-01 00:32:56 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,67 @@
+//text change
+
+function textChange(){
+
+	document.getElementById('h2').innerHTML="Hello World"
+}
+
+// text change2
+
+function textChange2(){
+
+	document.getElementById('ch2').innerHTML="Hi There"
+}
+
+// text change3
+
+function textChange3(){
+
+	document.getElementById('ch3').innerHTML="Hello World"
+}
+
+// text change4
+
+function textChange4(){
+
+	document.getElementById('ch4').innerHTML="Get Ready"
+}
+
+// text change5
+
+function textChange5(){
+	document.getElementById('ch5').innerHTML="Be Aware"
+}
+
+//text change6
+
+function textChange6(){
+
+	document.getElementById('ch6').innerHTML="Hello World"
+}
+
+// text change7
+
+function textChange7(){
+
+	document.getElementById('ch7').innerHTML="Hi There"
+}
+
+// text change8
+
+function textChange8(){
+
+	document.getElementById('ch8').innerHTML="Hello World"
+}
+
+// text change9
+
+function textChange9(){
+
+	document.getElementById('ch9').innerHTML="Get Ready"
+}
+
+// text change10
+
+function textChange10(){
+	document.getElementById('ch10').innerHTML="Be Aware"
+}
